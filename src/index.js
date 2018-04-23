8be798ec467c9ec94f43528d3e6d4808bed8f2a9
Hazrat Ali <iloveyii@yahoo.com>
2018-04-23 11:59:10 +0200

    added redux store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+    if(action.type === 'changeState') {
+        return action.payload.newState;
+    }
+    return 'State';
+}
+const store = createStore(reducer);
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New State'
+    }
+};
+
+store.dispatch(action);
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
