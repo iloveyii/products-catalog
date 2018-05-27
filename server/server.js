e79b4a0af6b616665ca801b580ee44a08d4d2629
Hazrat Ali <ali@triggybet.com>
2018-05-27 11:42:17 +0200

    Added mongo, express server

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const dbUrl = 'mongodb://localhost/products';
+
+// app setup
+var app = express();
+app.listen(4000, () => console.log('Express server started listening on port 4000'));
+
+
+MongoClient.connect(dbUrl, function (err, db) {
+    console.log('Mongodb connected');
+    app.get('/api/v1/products', (req, res) => {
+        console.log('Request: /api/v1/products');
+        db.collection('triggers').find( {}).toArray( (err, triggers) => {
+            res.json(triggers);
+        });
+    });
+
+    app.get('/api/v1/products/:id', (req, res) => {
+        console.log('request: ', req.params.id)
+        console.log('Request: /api/v1/products/:id');
+        db.collection('triggers').find( {_id: req.params.id}).toArray( (err, triggers) => {
+            res.json(triggers);
+        });
+    });
+
+
+    app.use((req, res) => {
+        console.log('Request:', req.url)
+        res.status(404).json({
+            errors: {
+                global: 'Some error, Don\'t worry a group of monkeys has been dispatched to fix it as soon as possible.'
+            }
+        })
+    })
+
+});
+
+
+
--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                {/*<header className="App-header">*/}
+                    {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+                    {/*<h1 className="App-title">Welcome to React</h1>*/}
+                {/*</header>*/}
 
                 <div className="ui container">
                     <div className="ui three item menu">
